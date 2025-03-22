06ab918be3b9e163d17142f94263e45e717c77b4
Manish Ydvv <mrmanishrao025@gmail.com>
2025-03-22 18:05:40 +0530

    first commit

---
+++ Notes.py
@@ -0,0 +1,18 @@
+# Write (conda) to check all info about conda(its all command)
+# (conda env list) -> To check the detail of the previous environment
+
+# conda create -p {name of the environment} python={version}  (-p)-> if we use this then we create environment in the local directory(Here Demo-Pipeline)
+# (-n) -> If we use this then it create it in the deafault directory (means in the env of the conda)
+
+# If we use (-n) so for activation directly use conda activate {name of the environment}
+# If we use (-p) so for activation use conda activate {path of the environement}(right click on env and copy the path and paste it)
+
+# Note for activation (hmre folders k name m space nhi hona chiye (jse Data science to ye ese alg alg argument smjega))
+
+# git remote -v -> To check that we are pointing to github repositary or not
+
+# git init -> For initialising the git repository
+
+# git add . -> For adding all the file in the git hub(We not want add any file then put the name of that file in the gitignore)
+
+# git reset -> To reset the change 
